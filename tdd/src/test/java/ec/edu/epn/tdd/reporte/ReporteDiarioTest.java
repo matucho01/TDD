3f8b8677ad78e02556cc3d3efd1d4f2b6e233357
Mateo Pérez <cristopher.perez01@epn.edu.ec>
2022-11-26 14:21:04 -0500

    Reload project Maven

--- tdd/src/test/java/ec/edu/epn/tdd/reporte/ReporteDiarioTest.java
+++ tdd/src/test/java/ec/edu/epn/tdd/reporte/ReporteDiarioTest.java
@@ -2,6 +2,7 @@ package ec.edu.epn.tdd.reporte;
 
 import org.junit.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -12,9 +13,8 @@ public class ReporteDiarioTest {
     @Test
     public void given_three_objects_when_addition_then_ok() {
         ReporteDiario reporteDiario = new ReporteDiario();
-        //ArrayList<Double> expected = new ArrayList<>(Arrays.asList(8529.78, , , ,));
-
-        //ArrayList<Double> actual = reporteDiario.alo();
-        //assertEquals(expected, actual);
+        ArrayList<Double> turnoManiana = new ArrayList<>();
+        ArrayList<Double> turnoTarde = new ArrayList<>();
+        ArrayList<Double> turnoNoche = new ArrayList<>();
     }
 }
