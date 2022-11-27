0dc2a3921cffe85f247bad503b4f6bca197e5808
Mateo Pérez <cristopher.perez01@epn.edu.ec>
2022-11-26 22:56:53 -0500

    Refactoring: Extract Method, @Before setUp()

--- tdd/src/test/java/ec/edu/epn/tdd/reporte/ReporteDiarioTest.java
+++ tdd/src/test/java/ec/edu/epn/tdd/reporte/ReporteDiarioTest.java
@@ -1,5 +1,6 @@
 package ec.edu.epn.tdd.reporte;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -11,9 +12,15 @@ import static org.junit.Assert.*;
 
 public class ReporteDiarioTest {
 
+    ReporteDiario reporteDiario = null;
+
+    @Before
+    public void setUp() {
+        reporteDiario = new ReporteDiario();
+    }
+
     @Test
     public void given_three_lists_when_addition_then_ok() {
-        ReporteDiario reporteDiario = new ReporteDiario();
         ArrayList<Double> expected = new ArrayList<>(Arrays.asList(15.62, 922.72, 823.88, 2288.08, 265.37, 10.50));
         ArrayList<Double> turnoManiana = new ArrayList<>(Arrays.asList(15.62, 319.32, 149.00, 983.45, 110.68, 7.0));
         ArrayList<Double> turnoTarde = new ArrayList<>(Arrays.asList(0.0, 507.24, 613.0, 1002.13, 118.12, 3.50));
@@ -24,7 +31,6 @@ public class ReporteDiarioTest {
 
     @Test(expected = IOException.class)
     public void given_a_list_when_creation_of_archive_then_exception(){
-        ReporteDiario reporteDiario = new ReporteDiario();
         ArrayList<Double> ventasTotales = new ArrayList<>(Arrays.asList(15.62, 922.72, 823.88, 2288.08, 265.37, 10.50));
         boolean actual = reporteDiario.generarArchivo(ventasTotales, "2022-11-25");
         assertTrue(actual);
